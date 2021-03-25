5d73d9f940f2cc8fff8dbe30620bb53261457674
OanaMG <oana_ghidiiu@yahoo.com>
2021-03-25 16:06:14 +0000

    working api - except update

--- RestaurantRepository.cs
+++ RestaurantRepository.cs
@@ -25,28 +25,26 @@ public class RestaurantRepository : BaseRepository, IRepository<Restaurant>
     public async Task<Restaurant> Get(int id)
     {
         using var connection = CreateConnection();
-        return await connection.QuerySingleAsync<Restaurant>("SELECT * FROM restaurant_capacity_view WHERE Id = @Id", new { Id = id });
+        return await connection.QuerySingleAsync<Restaurant>("SELECT * FROM restaurant_capacity_view WHERE Id = @Id;", new { Id = id });
     }
 
     public void Delete(int id)
     {
         using var connection = CreateConnection();
-        connection.Execute("DELETE * FROM restaurant_capacity_view WHERE Id = @Id", new { Id = id });
+        connection.Execute("DELETE FROM restaurant_capacity_view WHERE Id = @Id;", new { Id = id });
     }
 
     public async Task<Restaurant> Update(Restaurant restaurant)
     {
         using var connection = CreateConnection();
-        return await connection.QuerySingleAsync<Restaurant>("UPDATE restaurant_capacity_view SET RestaurantName = @RestaurantName, Description = @Description, OpeningTimes = @OpeningTimes, ClosingTimes = @ClosingTimes, PhoneNumber = @PhoneNumber, AddressLine1 = @AddressLine1, Area = @Area, Postcode =@Postcode, WebsiteURL =@WebsiteURL, PhotoURL = @PhotoURL, Capacity =@Capacity, Tables2 =@Tables2, Tables4 =@Tables4, Tables6 =@Tables6, Tables8 =@Tables8,  WHERE Id = @Id RETURNING *", restaurant);
-
+        return await connection.QuerySingleAsync<Restaurant>("UPDATE restaurant_capacity_view SET RestaurantName = @RestaurantName, Description = @Description, OpeningTimes = @OpeningTimes, ClosingTimes = @ClosingTimes, PhoneNumber = @PhoneNumber, AddressLine1 = @AddressLine1, Area = @Area, Postcode =@Postcode, WebsiteURL =@WebsiteURL, PhotoURL = @PhotoURL, Capacity =@Capacity, Tables2 =@Tables2, Tables4 =@Tables4, Tables6 =@Tables6, Tables8 =@Tables8,  WHERE Id = @Id RETURNING *;", restaurant);
     }
 
     public async Task<Restaurant> Insert(Restaurant restaurant)
     {
         using var connection = CreateConnection();
         return await connection.QuerySingleAsync<Restaurant>(
-            "INSERT INTO restaurant_capacity_view (RestaurantName, Description, OpeningTimes, ClosingTimes, PhoneNumber, AddressLine1, Area, Postcode, WebsiteURL, PhotoURL, Capacity, Tables2, Tables4, Tables6, Tables8) VALUES ((@RestaurantName, @Description, @OpeningTimes, @ClosingTimes, @PhoneNumber, @AddressLine1, @Area, @Postcode, @WebsiteURL, @PhotoURL, @Capacity, @Tables2, @Tables4, @Tables6, @Tables8) RETURNING *;", restaurant);
-
+            "INSERT INTO restaurant_capacity_view (RestaurantName, Description, OpeningTimes, ClosingTimes, PhoneNumber, AddressLine1, Area, Postcode, WebsiteURL, PhotoURL, Capacity, Tables2, Tables4, Tables6, Tables8) VALUES (@RestaurantName, @Description, @OpeningTimes, @ClosingTimes, @PhoneNumber, @AddressLine1, @Area, @Postcode, @WebsiteURL, @PhotoURL, @Capacity, @Tables2, @Tables4, @Tables6, @Tables8) RETURNING *;", restaurant);
     }
 
 
