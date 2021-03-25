e056b2e3e939fff2b627834d094d27c753acb499
OanaMG <oana_ghidiiu@yahoo.com>
2021-03-25 12:13:15 +0000

    base repository & irepository set up

---
+++ IRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IRepository<T> 
+{
+    Task<IEnumerable<T>> GetAll();
+    Task<IEnumerable<T>> GetSearch(string search);
+    Task<T> Get(int id);
+    Task<T> Insert (T t);
+    Task<T> Update (T t);
+    void Delete(long id);
+}
