11e038b09c59ecbefbf98e89c4b9f6b2617e81ea
OanaMG <oana_ghidiiu@yahoo.com>
2021-03-25 10:38:45 +0000

    initialize

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Collections.Specialized;
+using Microsoft.AspNetCore.Http;
+
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("restaurants")]
+    public class RestaurantController : ControllerBase
+    {
+       private readonly IRepository<Book> _bookRepository;
+
+        public RestaurantController(IRepository<Restaurant> restaurantRepository)
+        {
+        _   restaurantRepository = restaurantRepository;
+        }
+    
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
